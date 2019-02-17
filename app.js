b4844dd892ed782dde017642e8d64fc608b3a433
unknown <yauma8610@gmail.com>
2019-02-17 15:23:58 +0700

    print hello world

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var express = require("express");
 var path = require("path");
-// var config = require("./config/database");
+var config = require("./config/database");
 var mongoose = require("mongoose");
 
 //connect to db
-// mongoose.connect(config.database);
-// var db = mongoose.connection;
-// db.on("error", console.error.bind (console, "connection error:"));
-// db.once("open", function() {
-//   // we're connected!
-//   console.log("Conected to database");
-// });
+mongoose.connect(config.database);
+var db = mongoose.connection;
+db.on("error", console.error.bind (console, "connection error:"));
+db.once("open", function() {
+  // we're connected
+  console.log("Conected to database");
+});
 
 var app = express();
 
@@ -20,6 +20,11 @@ app.set("view engine", "ejs");
 
 // Set public folder
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', function (req, res) {
+    res.send('hello world')
+  })
+
 //setup server
 var port = 5000;
 app.listen(port, function(){
