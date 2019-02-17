37b32e2999f374b419a55de74d2c072f8376db6d
unknown <yauma8610@gmail.com>
2019-02-17 15:13:47 +0700

    installing mongoose and connect to database

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 var express = require("express");
 var path = require("path");
+// var config = require("./config/database");
+var mongoose = require("mongoose");
+
+//connect to db
+// mongoose.connect(config.database);
+// var db = mongoose.connection;
+// db.on("error", console.error.bind (console, "connection error:"));
+// db.once("open", function() {
+//   // we're connected!
+//   console.log("Conected to database");
+// });
 
 var app = express();
+
+// View engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Set public folder
+app.use(express.static(path.join(__dirname, "public")));
 //setup server
-var port = 3000;
+var port = 5000;
 app.listen(port, function(){
     console.log("Server sukses listen to " + port);
 });
